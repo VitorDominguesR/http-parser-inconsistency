fadb36d9167762ab2095fa61c86edf36ee131e1f
Vitor <vitor.ribeiro>
2024-02-22 17:09:14 +0000

    Changes on PHP application

--- php/src/index.php
+++ php/src/index.php
@@ -1,7 +1,5 @@
 <?php
 
-echo "Hello, World from Docker! <br>";
-echo "Hello ECS! <br>";
-echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
+echo "Hello, World from PHP! <br>";
 
 ?>
